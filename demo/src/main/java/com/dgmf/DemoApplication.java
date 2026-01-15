2647473cfb3354915fcbff758c6d1534557394e2
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2026-01-15 08:43:45 +0100

    Rename Root Package

--- demo/src/main/java/com/dgmf/DemoApplication.java
+++ demo/src/main/java/com/dgmf/DemoApplication.java
@@ -1,4 +1,4 @@
-package com.dgmf.demo;
+package com.dgmf;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- demo/src/test/java/com/dgmf/DemoApplicationTests.java
+++ demo/src/test/java/com/dgmf/DemoApplicationTests.java
@@ -1,4 +1,4 @@
-package com.dgmf.demo;
+package com.dgmf;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
